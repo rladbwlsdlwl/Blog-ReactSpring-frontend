487cb9b267364048b2147239537a97d7323ee2a8
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2024-07-30 17:51:36 +0900

    Feat: 로그인 페이지 기능 구현

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,13 +1,110 @@
 import axios from 'axios'
-import {useContext, useState} from 'react'
+import { useContext, useState } from 'react'
+import { urlpath } from '../utils/apiUtils'
+import googleImg from "../images/google.png"
+import "../css/login.css"
+import "../css/common.css"
+import { Link } from 'react-router-dom'
 import { AuthContext } from '../context/AuthProvider'
 
 export default function Login(){
     const googleurl = "http://localhost:8080/oauth2/authorization/google"
-    
+    const url = urlpath + "/signin"
+
+    const { settingToken } = useContext(AuthContext)
+
+
+    const [errorMsg, setErrorMsg] = useState("")
+    const [user, setUser] = useState({
+        username: "",
+        password: ""
+    })
+
+
+
+    function handleUserSetting(e){
+        const {name, value} = e.target
+
+        setUser({...user, [name]: value})
+    }
+
+    function submitHandler(){
+        const data = {
+            name: user.username,
+            password: user.password
+        }
+
+        axios.post(url, data).then(res => {
+            // header 값 저장
+            const headers = res.headers
+            const token = headers["authentication"] || headers["Authentication"] // 소문자로 들어옴..
+
+            settingToken(token)
+            
+            // 로그인 성공 알럿, 홈 화면 전환
+            window.alert("로그인 성공! 홈 화면으로 이동합니다")
+            window.location.pathname = "/"
+        }).catch(err => {
+            console.log(err)
+            const msg = err.response.data.message
+
+            setErrorMsg(msg)
+        })
+    }
     return (
-        <div>
-            <a href = {googleurl}>google login</a>
+        <div className = "signinContainer">
+            <div className = "signinOuter">
+                <div className = "inputOuterBox">
+                    <div className='inputOuterBox'>
+                        아이디/닉네임
+                    </div>
+                    <input className='inputBox' type = "text" placeholder='닉네임을 입력하세요' name = "username" value = {user.username} onChange = {handleUserSetting}></input>
+                </div>
+                <div className = 'inputOuterBox'> 
+                    <div className='inputOuterBox'>
+                        비밀번호
+                    </div>
+                    <input className='inputBox' type = "password" placeholder='비밀번호를 입력하세요' name = "password" value = {user.password} onChange = {handleUserSetting}></input>
+                </div>
+                
+                {/* 에러 메세지 */}
+                <div style = {{height: "50px"}}>
+                    {errorMsg}
+                </div>
+                
+
+                {/* 로그인 버튼 */}
+                <div className = 'inputOuterBox'>
+                    <input className = "inputBox" type = "submit" value = "로그인" onClick = {submitHandler}></input>
+                </div>
+
+                {/* 유틸리티 버튼 */}
+                <div className = 'inputOuterBox utilOuter'>
+                    <div className="util">
+                        <Link to = "/auth/login/findname" className = "link utilInner">
+                            아이디 찾기
+                        </Link>
+                    </div>
+                    <div className="util">
+                        <Link to = "/auth/login/findpassword" className = "link utilInner">
+                            비밀번호 찾기
+                        </Link>
+                    </div>
+                    <div className="util">
+                        <Link to = "/auth/signup" className = "link utilInner">
+                            회원가입
+                        </Link>
+                    </div>
+                </div>
+
+                {/* 소셜 로그인 버튼 */}
+                <div className='inputOuterBox'>
+                    <img src = {googleImg} onClick = {() => window.location.href = googleurl} />
+                </div>
+            </div>
+
+
+            
         </div>
     )
 }
